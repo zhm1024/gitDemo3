99159f3b52cfbd9d7727b832c264340f8db8e8d3
zhangmin <1214348423@qq.com>
2019-01-22 15:28:34 +0800

    newBranch2 commit 3

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
         System.out.println("add");
         System.out.println("this is newBranch2");
         System.out.println("this is newBranch2 add");
+        System.out.println("this is newBranch2 add2");
     }
 }
